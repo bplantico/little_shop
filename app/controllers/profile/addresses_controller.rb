035b950b849a64703313e3b0e33eaf0a7484298c
Brian Plantico <bplantico@gmail.com>
2019-06-01 22:25:52 -0600

    Adds profile_addresses_path along with Profile::AddressesController, all tests passing

---
+++ app/controllers/profile/addresses_controller.rb
@@ -0,0 +1,9 @@
+class Profile::AddressesController < ApplicationController
+
+  def index
+    # require "pry"; binding.pry
+    # @user = current_user
+    # @addresses = current_user.addresses
+  end
+
+end
